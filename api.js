7e46234979914c927275cf92c70ca195f3f93060
Kandukuri-Rajesh <rajeshkandukuri1221@gmail.com>
2024-11-25 10:03:45 -0600

    Update api.js

--- api.js
+++ api.js
@@ -44,14 +44,16 @@ async function getProduct(req, res, next) {
   return res.json(product)
 }
 
+
 /**
- * Create a product
- * @param {object} req 
- * @param {object} res 
+ * Create a new product
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
  */
-async function createProduct(req, res) {
-  console.log('request body:', req.body)
-  res.json(req.body)
+async function createProduct (req, res, next) {
+  const product = await Products.create(req.body)
+  res.json(product)
 }
 
 /**
